3ee6d69e36a7634960d6b5daba59f1d3a30eb07c
Greta Parks <gretaparks@gmail.com>
2018-12-13 16:50:52 -0700

    working commit: playing with colors

--- custom.js
+++ custom.js
@@ -1,4 +1,5 @@
 
+//initialize sidenav
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     // var instances = M.Sidenav.init(elems, options);
@@ -9,21 +10,3 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function(){
     $('.tabs').tabs();
 });
-
-
-
-
-
-
-// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-// var collapsibleElem = document.querySelector('.collapsible');
-// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-// Or with jQuery
-//
-// $(document).ready(function(){
-//     $('.sidenav').sidenav();
-// });
-/**
- * Created by greta on 12/10/2018.
- */
