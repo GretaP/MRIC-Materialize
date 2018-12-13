1b0128a85eee14dee98d6062ef5203fa84cfcbfd
Greta Parks <gretaparks@gmail.com>
2018-12-12 17:12:08 -0700

    working, changing layout and adding navigation

--- custom.js
+++ custom.js
@@ -4,6 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
     // var instances = M.Sidenav.init(elems, options);
 });
 
+
+//Initialize tabs
+$(document).ready(function(){
+    $('.tabs').tabs();
+});
+
+
+
+
+
+
 // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
 // var collapsibleElem = document.querySelector('.collapsible');
 // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
