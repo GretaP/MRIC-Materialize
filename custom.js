33d8343b692e71141e87cc6499be607825131c37
Greta Parks <gretaparks@gmail.com>
2019-01-09 12:00:02 -0700

    Parallax mini clean ups.  css clean, change brackets. add parallax init to custom, remove script from html.

--- custom.js
+++ custom.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // var instances = M.Sidenav.init(document.querySelectorAll('.sidenav'), options);
+    M.Sidenav.init(document.querySelectorAll('.sidenav'));
     M.ScrollSpy.init(document.querySelectorAll('.scrollspy'));
     M.Modal.init(document.querySelectorAll('.modal'));
     M.Tabs.init(document.querySelectorAll('.tabs'));
+    M.Parallax.init(document.querySelectorAll('.parallax'));
 });
